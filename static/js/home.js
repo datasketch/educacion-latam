063e6119fd304b4c700d58a77a30baa05fbe92f7
David Daza <03dazal@gmail.com>
2018-11-29 13:31:26 -0500

    Toggle between reviews

--- static/js/home.js
+++ static/js/home.js
@@ -1,7 +1,8 @@
 const storiesTabs = document.querySelector('.stories__tabs')
-const stages = document.querySelector('.stages')
+const reviews = document.querySelector('.reviews')
 const storyAuthor = document.querySelector('.story__author')
 const storyContent = document.querySelector('.story__content')
+const switchGroup = document.querySelector('.switch__group')
 let activeTab = document.querySelector('.story__tab.active')
 let activeSummary = document.querySelector('.summary.active')
 
@@ -16,14 +17,14 @@ request.onreadystatechange = () => {
   if (request.readyState === XMLHttpRequest.DONE && request.status === 200) {
     fragments = JSON.parse(request.responseText)
     groupedByTopic = groupBy(fragments.data, 'tema')
+    groupedByTopic.key = "topics"
     groupedByCountry = groupBy(fragments.data, 'país')
-    // stagesTemplate(groupedByTopic).forEach(template => stages.appendChild(template))
-    // templateListener(groupedByTopic)
-    countriesTemplate(groupedByCountry).forEach(template => stages.appendChild(template))
-    templateListener(groupedByCountry)
+    groupedByCountry.key = "countries"
+    appendReviewToDOM(groupedByTopic)
   }
 }
 
+// Handles the stories tabs interactivity
 storiesTabs.addEventListener('click', event => {
   const element = event.target
   if (!element.matches('.story__tab') || element === activeTab) {
@@ -45,12 +46,31 @@ storiesTabs.addEventListener('click', event => {
   activeSummary.classList.toggle('active')
 })
 
-function humanize (str) {
-  return str.replace(/-/g, ' ').split(' ').map(w => w.charAt(0).toUpperCase() + w.substr(1).toLowerCase()).join(' ')
+// Handles the reviews interactivity
+function showReviewByTopic (button) {
+  if (button.classList.contains('active')) {
+    return
+  }
+  reviews.innerHTML = ''
+  reviews.removeEventListener('click', handleBullets)
+  document.querySelector('.switch.active').classList.remove('active')
+  button.classList.add('active')
+  appendReviewToDOM(groupedByTopic)
 }
 
-function dehumanize (str) {
-  return str.replace(/\s/g, '-').split(' ').map(w => w.toLowerCase()).join('-')
+function showReviewByCountry (button) {
+  if (button.classList.contains('active')) {
+    return
+  }
+  reviews.innerHTML = ''
+  reviews.removeEventListener('click', handleBullets)
+  document.querySelector('.switch.active').classList.remove('active')
+  button.classList.add('active')
+  appendReviewToDOM(groupedByCountry)
+}
+
+function humanize (str) {
+  return str.replace(/-/g, ' ').split(' ').map(w => w.charAt(0).toUpperCase() + w.substr(1).toLowerCase()).join(' ')
 }
 
 function groupBy (array, field) {
@@ -61,6 +81,12 @@ function groupBy (array, field) {
   }, {})
 }
 
+function appendReviewToDOM (object) {
+  reviewsTemplate(object).forEach(template => reviews.appendChild(template))
+  templateListener(object)
+}
+
+// Made for request a country flag properly
 function createCountriesDictionary (object) {
   let dictionary = {}
   for (const key in object) {
@@ -79,111 +105,84 @@ function makeRequest () {
   request.send()
 }
 
-function stagesTemplate (object) {
+// Build reviews template
+function reviewsTemplate (object) {
+  const dictionary = createCountriesDictionary(fragments)
   const templates = []
   for (const key in object) {
-    if (object.hasOwnProperty(key)) {
-      const stage = document.createElement('div')
-      stage.classList.add('stage')
+    if (object.hasOwnProperty(key) && key !== "" && Array.isArray(object[key])) {
+      const review = document.createElement('div')
+      review.classList.add('review')
 
-      const stageTitle = document.createElement('p')
-      stageTitle.classList.add('stage__title')
-      stageTitle.textContent = humanize(key)
+      const reviewTitle = document.createElement('p')
+      reviewTitle.classList.add('review__title')
+      reviewTitle.textContent = humanize(key)
 
-      const stageFragments = document.createElement('ul')
-      stageFragments.classList.add('stage__fragments')
+      const reviewFragments = document.createElement('ul')
+      reviewFragments.classList.add('review__fragments')
 
       object[key].forEach(function (item) {
-        const stageBullet = document.createElement('li')
-        stageBullet.classList.add('stage__bullet')
-        stageBullet.dataset.id = item.id
-        stageBullet.dataset.key = key
-        stageFragments.appendChild(stageBullet)
+        const reviewBullet = document.createElement('li')
+        reviewBullet.classList.add('review__bullet')
+        reviewBullet.dataset.id = item.id
+        reviewBullet.dataset.key = key
+        reviewFragments.appendChild(reviewBullet)
       })
 
-      const stageImage = document.createElement('div')
-      stageImage.classList.add('stage__image')
+      const reviewImage = document.createElement('div')
+      reviewImage.classList.add('review__image')
 
       const image = document.createElement('img')
-      image.setAttribute('src', `images/${key}.png`)
+      
+      if (object.key === 'countries') {
+        image.setAttribute('src', `https://www.countryflags.io/${dictionary[key]}/flat/64.png`)
+      } else {
+        image.setAttribute('src', `images/${key}.png`)
+      }
 
-      stageImage.appendChild(image)
+      reviewImage.appendChild(image)
 
-      stage.appendChild(stageTitle)
-      stage.appendChild(stageFragments)
-      stage.appendChild(stageImage)
+      review.appendChild(reviewTitle)
+      review.appendChild(reviewFragments)
+      review.appendChild(reviewImage)
 
-      templates.push(stage)
+      templates.push(review)
     }
   }
   return templates
 }
 
+// Handles interactivity within the review template
+let activeBullet, key, item, actualObject
+
 function templateListener (object) {
+  actualObject = object
   // Set active bullet
-  let activeBullet = document.querySelector('.stage__bullet')
-  let key = activeBullet.dataset.key
-  let item = object[key].find(item => item.id === +activeBullet.dataset.id)
+  activeBullet = document.querySelector('.review__bullet')
+  key = activeBullet.dataset.key
+  item = object[key].find(item => item.id === +activeBullet.dataset.id)
   activeBullet.classList.add('active')
   storyAuthor.textContent = item.autor
   storyContent.textContent = item.texto
 
-  stages.addEventListener('click', event => {
-    const element = event.target
-    if (!element.matches('.stage__bullet')) {
-      return
-    }
-    // Update active bullet
-    activeBullet.classList.toggle('active')
-    activeBullet = element
-    activeBullet.classList.toggle('active')
-
-    key = activeBullet.dataset.key
-    item = object[key].find(item => item.id === +activeBullet.dataset.id)
-    storyAuthor.textContent = item.autor
-    storyContent.textContent = item.texto
-  })
+  reviews.addEventListener('click', handleBullets)
 }
 
-function countriesTemplate (object) {
-  const dictionary = createCountriesDictionary(fragments)
-  const templates = []
-  for (const key in object) {
-    if (object.hasOwnProperty(key) && key !== "") {
-      const stage = document.createElement('div')
-      stage.classList.add('stage')
-
-      const stageTitle = document.createElement('p')
-      stageTitle.classList.add('stage__title')
-      stageTitle.textContent = humanize(key)
-
-      const stageFragments = document.createElement('ul')
-      stageFragments.classList.add('stage__fragments')
-
-      object[key].forEach(function (item) {
-        const stageBullet = document.createElement('li')
-        stageBullet.classList.add('stage__bullet')
-        stageBullet.dataset.id = item.id
-        stageBullet.dataset.key = key
-        stageFragments.appendChild(stageBullet)
-      })
-
-      const stageImage = document.createElement('div')
-      stageImage.classList.add('stage__image')
-
-      const image = document.createElement('img')
-      image.setAttribute('src', `https://www.countryflags.io/${dictionary[key]}/flat/64.png`)
-
-      stageImage.appendChild(image)
-
-      stage.appendChild(stageTitle)
-      stage.appendChild(stageFragments)
-      stage.appendChild(stageImage)
-
-      templates.push(stage)
-    }
+function handleBullets (event) {
+  const element = event.target
+  if (!element.matches('.review__bullet') || element === activeBullet) {
+    return
   }
-  return templates
+  // Update active bullet
+  activeBullet.classList.toggle('active')
+  activeBullet = element
+  activeBullet.classList.toggle('active')
+
+  key = activeBullet.dataset.key
+  item = actualObject[key].find(item => item.id === +activeBullet.dataset.id)
+  storyAuthor.textContent = item.autor
+  storyContent.textContent = item.texto
 }
+
 
 makeRequest()
