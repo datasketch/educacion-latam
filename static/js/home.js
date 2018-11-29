17ab675d27a0be1c30a7cfce9833bef696e54a20
David Daza <03dazal@gmail.com>
2018-11-29 11:51:42 -0500

    Interactive bullets by country

--- static/js/home.js
+++ static/js/home.js
@@ -5,93 +5,6 @@ const storyContent = document.querySelector('.story__content')
 let activeTab = document.querySelector('.story__tab.active')
 let activeSummary = document.querySelector('.summary.active')
 
-const humanize = str => {
-  return str.replace(/-/g, ' ').split(' ').map(w => w.charAt(0).toUpperCase() + w.substr(1).toLowerCase()).join(' ')
-}
-
-const dehumanize = str => {
-  return str.replace(/\s/g, '-').split(' ').map(w => w.toLowerCase()).join('-')
-}
-
-const groupBy = (array, field) => {
-  return array.reduce((group, item) => {
-    group[item[field]] = group[item[field]] || []
-    group[item[field]].push(item)
-    return group
-  }, {})
-}
-
-const makeRequest = () => {
-  request.open('GET', url)
-  request.send()
-}
-
-const stagesTemplate = o => {
-  const templates = []
-  for (const key in o) {
-    if (o.hasOwnProperty(key)) {
-      const stage = document.createElement('div')
-      stage.classList.add('stage')
-
-      const stageTitle = document.createElement('p')
-      stageTitle.classList.add('stage__title')
-      stageTitle.textContent = humanize(key)
-
-      const stageFragments = document.createElement('ul')
-      stageFragments.classList.add('stage__fragments')
-
-      o[key].forEach(function (item, index) {
-        const stageBullet = document.createElement('li')
-        stageBullet.classList.add('stage__bullet')
-        stageBullet.dataset.author = dehumanize(item.autor)
-        stageBullet.dataset.key = key
-        stageFragments.appendChild(stageBullet)
-      })
-
-      const stageImage = document.createElement('div')
-      stageImage.classList.add('stage__image')
-
-      const image = document.createElement('img')
-      image.setAttribute('src', `images/${key}.png`)
-
-      stageImage.appendChild(image)
-
-      stage.appendChild(stageTitle)
-      stage.appendChild(stageFragments)
-      stage.appendChild(stageImage)
-
-      templates.push(stage)
-    }
-  }
-  return templates
-}
-
-const templateListener = (object) => {
-  // Set active bullet
-  let activeBullet = document.querySelector('.stage__bullet')
-  let key = activeBullet.dataset.key
-  let author = object[key].find(item => dehumanize(item.autor) === activeBullet.dataset.author)
-  activeBullet.classList.add('active')
-  storyAuthor.textContent = author.autor
-  storyContent.textContent = author.texto
-
-  stages.addEventListener('click', event => {
-    const element = event.target
-    if (!element.matches('.stage__bullet')) {
-      return
-    }
-    // Update active bullet
-    activeBullet.classList.toggle('active')
-    activeBullet = element
-    activeBullet.classList.toggle('active')
-
-    key = activeBullet.dataset.key
-    author = object[key].find(item => dehumanize(item.autor) === activeBullet.dataset.author)
-    storyAuthor.textContent = author.autor
-    storyContent.textContent = author.texto
-  })
-}
-
 /* HTTP Request */
 let fragments, groupedByTopic, groupedByCountry
 
@@ -104,8 +17,10 @@ request.onreadystatechange = () => {
     fragments = JSON.parse(request.responseText)
     groupedByTopic = groupBy(fragments.data, 'tema')
     groupedByCountry = groupBy(fragments.data, 'país')
-    stagesTemplate(groupedByTopic).forEach(template => stages.appendChild(template))
-    templateListener(groupedByTopic)
+    // stagesTemplate(groupedByTopic).forEach(template => stages.appendChild(template))
+    // templateListener(groupedByTopic)
+    countriesTemplate(groupedByCountry).forEach(template => stages.appendChild(template))
+    templateListener(groupedByCountry)
   }
 }
 
@@ -129,5 +44,146 @@ storiesTabs.addEventListener('click', event => {
   activeSummary = summary
   activeSummary.classList.toggle('active')
 })
+
+function humanize (str) {
+  return str.replace(/-/g, ' ').split(' ').map(w => w.charAt(0).toUpperCase() + w.substr(1).toLowerCase()).join(' ')
+}
+
+function dehumanize (str) {
+  return str.replace(/\s/g, '-').split(' ').map(w => w.toLowerCase()).join('-')
+}
+
+function groupBy (array, field) {
+  return array.reduce((group, item) => {
+    group[item[field]] = group[item[field]] || []
+    group[item[field]].push(item)
+    return group
+  }, {})
+}
+
+function createCountriesDictionary (object) {
+  let dictionary = {}
+  for (const key in object) {
+    if (object.hasOwnProperty(key)) {
+      object[key].reduce((table, item) => {
+        table[item['país']] = item.iso.toLowerCase()
+        return table
+      }, dictionary)
+    }
+  }
+  return dictionary
+}
+
+function makeRequest () {
+  request.open('GET', url)
+  request.send()
+}
+
+function stagesTemplate (object) {
+  const templates = []
+  for (const key in object) {
+    if (object.hasOwnProperty(key)) {
+      const stage = document.createElement('div')
+      stage.classList.add('stage')
+
+      const stageTitle = document.createElement('p')
+      stageTitle.classList.add('stage__title')
+      stageTitle.textContent = humanize(key)
+
+      const stageFragments = document.createElement('ul')
+      stageFragments.classList.add('stage__fragments')
+
+      object[key].forEach(function (item) {
+        const stageBullet = document.createElement('li')
+        stageBullet.classList.add('stage__bullet')
+        stageBullet.dataset.id = item.id
+        stageBullet.dataset.key = key
+        stageFragments.appendChild(stageBullet)
+      })
+
+      const stageImage = document.createElement('div')
+      stageImage.classList.add('stage__image')
+
+      const image = document.createElement('img')
+      image.setAttribute('src', `images/${key}.png`)
+
+      stageImage.appendChild(image)
+
+      stage.appendChild(stageTitle)
+      stage.appendChild(stageFragments)
+      stage.appendChild(stageImage)
+
+      templates.push(stage)
+    }
+  }
+  return templates
+}
+
+function templateListener (object) {
+  // Set active bullet
+  let activeBullet = document.querySelector('.stage__bullet')
+  let key = activeBullet.dataset.key
+  let item = object[key].find(item => item.id === +activeBullet.dataset.id)
+  activeBullet.classList.add('active')
+  storyAuthor.textContent = item.autor
+  storyContent.textContent = item.texto
+
+  stages.addEventListener('click', event => {
+    const element = event.target
+    if (!element.matches('.stage__bullet')) {
+      return
+    }
+    // Update active bullet
+    activeBullet.classList.toggle('active')
+    activeBullet = element
+    activeBullet.classList.toggle('active')
+
+    key = activeBullet.dataset.key
+    item = object[key].find(item => item.id === +activeBullet.dataset.id)
+    storyAuthor.textContent = item.autor
+    storyContent.textContent = item.texto
+  })
+}
+
+function countriesTemplate (object) {
+  const dictionary = createCountriesDictionary(fragments)
+  const templates = []
+  for (const key in object) {
+    if (object.hasOwnProperty(key) && key !== "") {
+      const stage = document.createElement('div')
+      stage.classList.add('stage')
+
+      const stageTitle = document.createElement('p')
+      stageTitle.classList.add('stage__title')
+      stageTitle.textContent = humanize(key)
+
+      const stageFragments = document.createElement('ul')
+      stageFragments.classList.add('stage__fragments')
+
+      object[key].forEach(function (item) {
+        const stageBullet = document.createElement('li')
+        stageBullet.classList.add('stage__bullet')
+        stageBullet.dataset.id = item.id
+        stageBullet.dataset.key = key
+        stageFragments.appendChild(stageBullet)
+      })
+
+      const stageImage = document.createElement('div')
+      stageImage.classList.add('stage__image')
+
+      const image = document.createElement('img')
+      image.setAttribute('src', `https://www.countryflags.io/${dictionary[key]}/flat/64.png`)
+
+      stageImage.appendChild(image)
+
+      stage.appendChild(stageTitle)
+      stage.appendChild(stageFragments)
+      stage.appendChild(stageImage)
+
+      templates.push(stage)
+    }
+  }
+  return templates
+}
 
 makeRequest()
